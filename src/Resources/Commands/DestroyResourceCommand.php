0f14714b57c0962b54ad89a56574fb383a59c1aa
Eugene Glotov <kivagant@gmail.com>
2016-06-03 19:15:09 +0300

    DestroyResourceCommand now used FindResourceOptionsCommand instead of shell command executing

--- src/Resources/Commands/DestroyResourceCommand.php
+++ src/Resources/Commands/DestroyResourceCommand.php
@@ -3,6 +3,7 @@ namespace Staticus\Resources\Commands;
 
 use League\Flysystem\FilesystemInterface;
 use Staticus\Resources\Exceptions\CommandErrorException;
+use Staticus\Resources\ResourceDOAbstract;
 use Staticus\Resources\ResourceDOInterface;
 
 class DestroyResourceCommand implements ResourceCommandInterface
@@ -36,48 +37,46 @@ class DestroyResourceCommand implements ResourceCommandInterface
         $namespace = $this->resourceDO->getNamespace();
         $filePath = $this->resourceDO->getFilePath();
         if (!$uuid || !$type || !$baseDir || !$filePath) {
-            throw new CommandErrorException('Invalid destroy request');
+            throw new CommandErrorException('Cannot destroy the empty resource');
         }
         if ($byPathOnly) {
-            if (!$this->filesystem->delete($filePath)) {
-                throw new CommandErrorException('The file cannot be removed: ' . $filePath);
-            }
+            $this->deleteFile($filePath);
         } else {
-            $command = $this->getShellFindCommand($baseDir, $namespace, $uuid, $type, $variant, $version);
-            $command .= ' -delete';
-            shell_exec($command . '> /dev/null 2>&1');
+            $command = new FindResourceOptionsCommand($this->resourceDO, $this->filesystem);
+            $result = $command();
+            foreach ($result as $item) {
+                if (
+                    $item[ResourceDOAbstract::TOKEN_TYPE] !== $type
+                    || $item['filename'] !== $uuid
+                    || ($namespace && ($item[ResourceDOAbstract::TOKEN_NAMESPACE] !== $namespace))
+                ) {
+                    continue;
+                }
+                if ($version !== ResourceDOInterface::DEFAULT_VERSION) {
+                    if (
+                        $variant === $item[ResourceDOAbstract::TOKEN_VARIANT] // delete versions only for current variant
+                        && $version === (int)$item[ResourceDOAbstract::TOKEN_VERSION]
+                    ) {
+                        $this->deleteFile($item['path']);
+                    }
+                } elseif ($variant !== ResourceDOInterface::DEFAULT_VARIANT) {
+                    if ($variant === $item[ResourceDOAbstract::TOKEN_VARIANT]) {
+                        $this->deleteFile($item['path']);
+                    }
+                } else {
+                    $this->deleteFile($item['path']);
+                }
+            }
         }
 
         return $this->resourceDO;
     }
 
-    /**
-     * @param $baseDir
-     * @param $namespace
-     * @param $uuid
-     * @param $type
-     * @param string $variant
-     * @param int $version
-     * @return string
-     * @deprecated
-     * @todo: replace to FlySystem
-     */
-    protected function getShellFindCommand($baseDir, $namespace, $uuid, $type, $variant = ResourceDOInterface::DEFAULT_VARIANT, $version = ResourceDOInterface::DEFAULT_VERSION)
+    protected function deleteFile($filePath)
     {
-        if ($namespace) {
-            $namespace .= DIRECTORY_SEPARATOR;
+        // If file is already gone somewhere, it is OK for us
+        if ($this->filesystem->has($filePath) && !$this->filesystem->delete($filePath)) {
+            throw new CommandErrorException('The file cannot be removed: ' . $filePath);
         }
-        $command = 'find ';
-        if ($version !== ResourceDOInterface::DEFAULT_VERSION) {
-            $command .= $baseDir . $namespace . $type . DIRECTORY_SEPARATOR . $variant . DIRECTORY_SEPARATOR . $version . DIRECTORY_SEPARATOR;
-        } elseif ($variant !== ResourceDOInterface::DEFAULT_VARIANT) {
-            $command .= $baseDir . $namespace . $type . DIRECTORY_SEPARATOR . $variant . DIRECTORY_SEPARATOR;
-        } else {
-            $command .= $baseDir . $namespace . $type . DIRECTORY_SEPARATOR;
-        }
-
-        $command .= ' -type f -name ' . $uuid . '.' . $type;
-
-        return $command;
     }
 }
